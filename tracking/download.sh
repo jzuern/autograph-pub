9e27289b97364379435b5085c9a3f922f6bf700f
zuern <zuern@informatik.uni-freiburg.de>
2023-01-18 15:23:19 +0100

    tracking pipeline implemented

---
+++ tracking/download.sh
@@ -0,0 +1,22 @@
+
+# download sensor dataset
+s5cmd --no-sign-request -numworkers 128 cp --exclude "*/cameras/*" "s3://argoai-argoverse/av2/sensor/test/*" /data/argoverse2-full/sensor/test
+s5cmd --no-sign-request -numworkers 128 cp --exclude "*/cameras/*" "s3://argoai-argoverse/av2/sensor/val/*" /data/argoverse2-full/sensor/val
+s5cmd --no-sign-request -numworkers 128 cp --exclude "*/cameras/*" "s3://argoai-argoverse/av2/sensor/train/*" /data/argoverse2-full/sensor/train
+
+# download lidar dataset
+s5cmd --no-sign-request -numworkers 128 cp --exclude "*/cameras/*" "s3://argoai-argoverse/av2/lidar/test/*" /data/argoverse2-full/lidar/test
+s5cmd --no-sign-request -numworkers 128 cp --exclude "*/cameras/*" "s3://argoai-argoverse/av2/lidar/val/*" /data/argoverse2-full/lidar/val
+s5cmd --no-sign-request -numworkers 128 cp --exclude "*/cameras/*" "s3://argoai-argoverse/av2/lidar/train/*" /data/argoverse2-full/lidar/train
+
+# download motion-forecasting dataset
+s5cmd --no-sign-request -numworkers 128 cp --exclude "*/cameras/*" "s3://argoai-argoverse/av2/motion-forecasting/test/*" /data/argoverse2-full/motion-forecasting/test
+s5cmd --no-sign-request -numworkers 128 cp --exclude "*/cameras/*" "s3://argoai-argoverse/av2/motion-forecasting/val/*" /data/argoverse2-full/motion-forecasting/val
+s5cmd --no-sign-request -numworkers 128 cp --exclude "*/cameras/*" "s3://argoai-argoverse/av2/motion-forecasting/train/*" /data/argoverse2-full/motion-forecasting/train
+
+# download tbv dataset
+s5cmd --no-sign-request -numworkers 128 cp --exclude "*/cameras/*" "s3://argoai-argoverse/av2/tbv/test/*" /data/argoverse2-full/sensor/test
+s5cmd --no-sign-request -numworkers 128 cp --exclude "*/cameras/*" "s3://argoai-argoverse/av2/tbv/val/*" /data/argoverse2-full/sensor/val
+s5cmd --no-sign-request -numworkers 128 cp --exclude "*/cameras/*" "s3://argoai-argoverse/av2/tbv/train/*" /data/argoverse2-full/sensor/train
+
+
--- tracking/tracking.py
+++ tracking/tracking.py
@@ -10,7 +10,10 @@ import time
 from nuscenes.utils import splits
 import numpy as np
 import copy
-
+import pickle
+from scipy.optimize import linear_sum_assignment
+from glob import glob
+import matplotlib.pyplot as plt
 
 
 def greedy_assignment(dist):
@@ -25,28 +28,27 @@ def greedy_assignment(dist):
   return np.array(matched_indices, np.int32).reshape(-1, 2)
 
 NUSCENES_TRACKING_NAMES = [
-    'bicycle',
-    'bus',
-    'car',
-    'motorcycle',
-    'pedestrian',
-    'trailer',
-    'truck'
+    1, # car
 ]
 
 # 99.9 percentile of the l2 velocity error distribution (per clss / 0.5 second)
 # This is an earlier statistcs and I didn't spend much time tuning it.
 # Tune this for your model should provide some considerable AMOTA improvement
+# NUSCENE_CLS_VELOCITY_ERROR = {
+#     'car': 4,
+#     'truck': 4,
+#     'bus': 5.5,
+#     'trailer': 3,
+#     'pedestrian': 1,
+#     'motorcycle': 13,
+#     'bicycle': 3,
+# }
 NUSCENE_CLS_VELOCITY_ERROR = {
-    'car': 4,
-    'truck': 4,
-    'bus': 5.5,
-    'trailer': 3,
-    'pedestrian': 1,
-    'motorcycle': 13,
-    'bicycle': 3,
+    1: 4, # car
 }
 
+
+SCORE_THRSHLD = 0.3
 
 class Tracker(object):
     def __init__(self, hungarian=False, max_age=0):
@@ -69,17 +71,46 @@ class Tracker(object):
             return []
         else:
             temp = []
-            for det in results:
+            detections = []
+
+            for i in range(len(results["pred_labels"])):
+
+                detection = {}
+                detection["label_preds"] = results["pred_labels"][i]
+                detection["score"] = results["pred_scores"][i]
+
+                box = results["pred_boxes"][i]
+                # format:
+                # center = box[0:3]
+                # lwh = box[3:6]
+                # axis_angles = np.array([0, 0, box[6] + 1e-10])
+                detection["translation"] = box[0:3]
+                detection["velocity"] = np.zeros([3])
+                detection["size"] = box[3:6]
+                detection["rotation"] = box[6]
+
+                detections.append(detection)
+
+
+            for det in detections:
                 # filter out classes not evaluated for tracking
-                if det['detection_name'] not in NUSCENES_TRACKING_NAMES:
+                if det['label_preds'] not in NUSCENES_TRACKING_NAMES:
+                    continue
+
+                # filter out low-score predictions
+                if det['score'] < SCORE_THRSHLD:
                     continue
 
                 det['ct'] = np.array(det['translation'][:2])
                 det['tracking'] = np.array(det['velocity'][:2]) * -1 * time_lag
-                det['label_preds'] = NUSCENES_TRACKING_NAMES.index(det['detection_name'])
                 temp.append(det)
 
             results = temp
+
+        if len(results) == 0:
+            self.tracks = []
+            return []
+
 
         N = len(results)
         M = len(self.tracks)
@@ -93,10 +124,11 @@ class Tracker(object):
             dets = np.array(
                 [det['ct'] for det in results], np.float32)
 
+
         item_cat = np.array([item['label_preds'] for item in results], np.int32)  # N
         track_cat = np.array([track['label_preds'] for track in self.tracks], np.int32)  # M
 
-        max_diff = np.array([self.NUSCENE_CLS_VELOCITY_ERROR[box['detection_name']] for box in results], np.float32)
+        max_diff = np.array([self.NUSCENE_CLS_VELOCITY_ERROR[box['label_preds']] for box in results], np.float32)
 
         tracks = np.array(
             [pre_det['ct'] for pre_det in self.tracks], np.float32)  # M x 2
@@ -106,24 +138,21 @@ class Tracker(object):
                       dets.reshape(-1, 1, 2)) ** 2).sum(axis=2))  # N x M
             dist = np.sqrt(dist)  # absolute distance in meter
 
-            invalid = ((dist > max_diff.reshape(N, 1)) + \
-                       (item_cat.reshape(N, 1) != track_cat.reshape(1, M))) > 0
+            invalid = ((dist > max_diff.reshape(N, 1)) + (item_cat.reshape(N, 1) != track_cat.reshape(1, M))) > 0
 
             dist = dist + invalid * 1e18
             if self.hungarian:
                 dist[dist > 1e18] = 1e18
-                matched_indices = linear_assignment(copy.deepcopy(dist))
+                ind = linear_sum_assignment(copy.deepcopy(dist))
+                matched_indices = np.vstack([ind[0], ind[1]]).T
             else:
                 matched_indices = greedy_assignment(copy.deepcopy(dist))
         else:  # first few frame
             assert M == 0
             matched_indices = np.array([], np.int32).reshape(-1, 2)
 
-        unmatched_dets = [d for d in range(dets.shape[0]) \
-                          if not (d in matched_indices[:, 0])]
-
-        unmatched_tracks = [d for d in range(tracks.shape[0]) \
-                            if not (d in matched_indices[:, 1])]
+        unmatched_dets = [d for d in range(dets.shape[0]) if not (d in matched_indices[:, 0])]
+        unmatched_tracks = [d for d in range(tracks.shape[0]) if not (d in matched_indices[:, 1])]
 
         if self.hungarian:
             matches = []
@@ -236,79 +265,125 @@ def save_first_frame():
 if __name__ == '__main__':
 
     args = parse_args()
-    print('Deploy OK')
 
     tracker = Tracker(max_age=args.max_age,
                       hungarian=True)
 
-    with open(args.checkpoint, 'rb') as f:
-        predictions = json.load(f)['results']
+    predictions_files = sorted(glob("/data/argoverse2-full/sensor/val/*/detections.pickle"))
 
-    with open(os.path.join(args.work_dir, 'frames_meta.json'), 'rb') as f:
-        frames = json.load(f)['frames']
 
-    nusc_annos = {
-        "results": {},
-        "meta": None,
-    }
+    for predictions_file in predictions_files:
+        print("Performing Tracking on predictions file {}".format(predictions_file))
 
-    size = len(frames)
+        tracking_file = predictions_file.replace("detections.pickle", "tracking.pickle")
 
-    print("Begin Tracking\n")
-    start = time.time()
-    for i in range(size):
-        token = frames[i]['token']
+        if os.path.isfile(tracking_file):
+            print("    skip. Existing.")
+            continue
 
-        # reset tracking after one video sequence
-        if frames[i]['first']:
-            # use this for sanity check to ensure your token order is correct
-            # print("reset ", i)
-            tracker.reset()
-            last_time_stamp = frames[i]['timestamp']
+        with open(predictions_file, "rb") as f:
+            predictions_dict = pickle.load(f)
 
-        time_lag = (frames[i]['timestamp'] - last_time_stamp)
-        last_time_stamp = frames[i]['timestamp']
+        av2_annos = {
+            "results": {},
+            "city_name": predictions_dict["city_name"],
+            "meta": None,
+        }
 
-        preds = predictions[token]
+        predictions = predictions_dict["predictions"]
 
-        outputs = tracker.step_centertrack(preds, time_lag)
-        annos = []
+        size = len(predictions)
 
-        for item in outputs:
-            if item['active'] == 0:
-                continue
-            nusc_anno = {
-                "sample_token": token,
-                "translation": item['translation'],
-                "size": item['size'],
-                "rotation": item['rotation'],
-                "velocity": item['velocity'],
-                "tracking_id": str(item['tracking_id']),
-                "tracking_name": item['detection_name'],
-                "tracking_score": item['detection_score'],
-            }
-            annos.append(nusc_anno)
-        nusc_annos["results"].update({token: annos})
 
-    end = time.time()
+        tracker.reset()
+        ego_pos = []
 
-    second = (end - start)
+        for i in range(size):
+            token = predictions[i]['fname_lidar']
+            ego_pos.append(predictions[i]['ego_pos'])
 
-    speed = size / second
-    print("The speed is {} FPS".format(speed))
+            # reset tracking after one video sequence
+            if i == 0:
+                # use this for sanity check to ensure your token order is correct
+                # print("reset ", i)
+                tracker.reset()
+                last_time_stamp = predictions[i]['timestamp']
 
-    nusc_annos["meta"] = {
-        "use_camera": False,
-        "use_lidar": True,
-        "use_radar": False,
-        "use_map": False,
-        "use_external": False,
-    }
+            time_lag = (predictions[i]['timestamp'] - last_time_stamp)
+            last_time_stamp = predictions[i]['timestamp']
 
-    res_dir = os.path.join(args.work_dir)
-    if not os.path.exists(res_dir):
-        os.makedirs(res_dir)
+            outputs = tracker.step_centertrack(predictions[i], time_lag)
+            annos = []
 
-    with open(os.path.join(args.work_dir, 'tracking_result.json'), "w") as f:
-        json.dump(nusc_annos, f)
+            for item in outputs:
+                if item['active'] == 0:
+                    continue
+                anno = {
+                    #"sample_token": token,
+                    "translation": item['translation'],
+                    "size": item['size'],
+                    "rotation": item['rotation'],
+                    "velocity": item['velocity'],
+                    "tracking_id": str(item['tracking_id']),
+                    #"tracking_name": item['detection_name'],
+                    #"tracking_score": item['detection_score'],
+                    "label": item['label_preds'],
+                }
+                annos.append(anno)
+            av2_annos["results"].update({token: annos})
+
+        av2_annos["ego_pos"] = ego_pos
+
+        av2_annos["meta"] = {
+            "use_camera": False,
+            "use_lidar": True,
+            "use_radar": False,
+            "use_map": False,
+            "use_external": False,
+        }
+
+        with open(tracking_file, "wb") as f:
+            pickle.dump(av2_annos, f)
+
+
+
+
+    #########################################################
+
+
+
+    # Visualize tracklets
+    predictions_files = sorted(glob("/data/argoverse2-full/sensor/val/*/tracking.pickle"))
+
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+
+
+    for p in predictions_files:
+        with open(p, "rb") as f:
+            av2_annos = pickle.load(f)
+
+        tracklets = {}
+
+        ego_pos = np.array([p.translation for p in av2_annos["ego_pos"]])
+        ax.plot(ego_pos[:, 0], ego_pos[:, 1], "k-")
+
+        if av2_annos["city_name"] == "PIT":
+
+            for counter, anno in enumerate(av2_annos["results"].keys()):
+                for t in av2_annos["results"][anno]:
+                    t_id = t["tracking_id"]
+                    t_trans = t["translation"]
+                    if t_id in tracklets.keys():
+                        tracklets[t_id].append(t_trans)
+                    else:
+                        tracklets[t_id] = [t_trans]
+
+            for tracklet in tracklets:
+                traj = np.array(tracklets[tracklet])
+                ax.plot(traj[:, 0], traj[:, 1])
+    plt.show()
+
+
+
 
