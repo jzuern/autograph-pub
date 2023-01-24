411ec0ebbbccc68536adbdc419e67db145ad547a
zuern <zuern@informatik.uni-freiburg.de>
2023-01-24 15:13:37 +0100

    update pipeline

--- aggregation/pipeline.sh
+++ aggregation/pipeline.sh
@@ -2,23 +2,16 @@ export PYTHONPATH=$PYTHONPATH:/home/zuern/self-supervised-graph
 
 # iterate over all cities
 #CITIES=(pittsburgh miami washington paloalto austin detroit)
-CITIES=(miami washington paloalto austin detroit)
-#CITIES=(pittsburgh)
+CITIES=(pittsburgh)
 
-EXP="exp-06-01-23-large"
+EXP="exp-successors-traj2"
 OUT_DIR="/data/autograph"
 
 
 
 for CITY in "${CITIES[@]}"; do
     echo "Processing $CITY !"
-    #~/anaconda3/envs/geometric/bin/python aggregate_av2.py --city_name $CITY --out_path_root $OUT_DIR/$EXP/$CITY-pre --source tracklets_sparse
-    ~/anaconda3/envs/geometric/bin/python aggregate_av2.py --city_name $CITY --out_path_root $OUT_DIR/$EXP/$CITY-pre --source tracklets_dense
-#    ~/anaconda3/envs/geometric/bin/python aggregate_av2.py --city_name $CITY --out_path_root $OUT_DIR/$EXP/$CITY-pre --source lanes
-#
-#    ~/anaconda3/envs/geometric/bin/python infer_regressor.py --out_path_root $OUT_DIR/$EXP/$CITY-pre --checkpoint ../checkpoints/pit/regressor_cosmic-sponge-2_0040-0.41017.pth
-#
-#    ~/anaconda3/envs/geometric/bin/python aggregate_av2.py --city_name $CITY --out_path_root $OUT_DIR/$EXP/$CITY-post --export_final --source tracklets_sparse
-#    ~/anaconda3/envs/geometric/bin/python aggregate_av2.py --city_name $CITY --out_path_root $OUT_DIR/$EXP/$CITY-post --export_final --source tracklets_dense
-#    ~/anaconda3/envs/geometric/bin/python aggregate_av2.py --city_name $CITY --out_path_root $OUT_DIR/$EXP/$CITY-post --export_final --source lanes
+    ~/anaconda3/envs/geometric/bin/python aggregate_av2.py --city_name $CITY --out_path_root $OUT_DIR/$EXP/$CITY-pre --source tracklets_dense --num_cpus 4
+    #~/anaconda3/envs/geometric/bin/python infer_regressor.py --out_path_root $OUT_DIR/$EXP/$CITY-pre --checkpoint ../checkpoints/pit/regressor_cosmic-sponge-2_0040-0.41017.pth
+    #~/anaconda3/envs/geometric/bin/python aggregate_av2.py --city_name $CITY --out_path_root $OUT_DIR/$EXP/$CITY-post --export_final --source tracklets_dense
 done
---
+++ aggregation/traj-merging-tool.py
@@ -0,0 +1,31 @@
+
+import numpy as np
+import cv2
+from aggregation.aggregate_av2 import merge_successor_trajectories, get_succ_graph
+import time
+
+
+
+if __name__ == '__main__':
+
+    t = np.load("trajectories_0.npy", allow_pickle=True)
+    sat_image = cv2.imread("sat_image.png")
+    sat_image = cv2.resize(sat_image, (sat_image.shape[1], sat_image.shape[0]))
+
+
+    def viz(event, mouseX, mouseY, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            now = time.time()
+            q = np.array([mouseX, mouseY])
+
+            succ_traj,  sat_image_viz = merge_successor_trajectories(q, t, sat_image)
+            sat_image_viz, sdf = get_succ_graph(q, succ_traj,  sat_image_viz)
+
+            sat_image_viz = cv2.circle(sat_image_viz, (mouseX, mouseY), 5, (0, 0, 0), -1)
+            print("Inference time: {:.4f} s".format(time.time() - now))
+            cv2.imshow('image_0', sat_image_viz)
+
+    cv2.namedWindow('image_0')
+    cv2.setMouseCallback('image_0', viz)
+    cv2.waitKey(1)
+    cv2.waitKey(0)
